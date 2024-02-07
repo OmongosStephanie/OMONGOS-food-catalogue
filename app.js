5852aac16e3db510ea3baa4d772842123dd13a45
stephanie <stephanieomongos05@gmail.com>
2024-02-07 16:27:59 +0800

    added files

---
+++ app.js
@@ -0,0 +1,29 @@
+const apiUrl = 'data.json';
+
+async function fetchAndDisplayProducts() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const productList = document.getElementById('product-list');
+
+        data.forEach(product => {
+            const productCard = document.createElement('div');
+            productCard.classList.add('product-card');
+
+            const productName = document.createElement('h3');
+            productName.textContent = product.name;
+
+            const productPrice = document.createElement('p');
+            productPrice.textContent = `Price: ₱${product.price}`;
+
+            productCard.appendChild(productName);
+            productCard.appendChild(productPrice);
+
+            productList.appendChild(productCard); 
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchAndDisplayProducts();
